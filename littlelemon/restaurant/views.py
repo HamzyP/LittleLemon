d05f74889f41609f143af8789feb9cc5bfe98085
HamzyP <134822658+HamzyP@users.noreply.github.com>
2025-10-04 23:46:23 +0100

    Added Serializers.py + core API functionality for table Booking and Menu. Added Routing for booking and Menu in the app level urls.py file and used DefaultRouter to configure BookingViewSet (GET PUT DELETE).

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets, permissions
+from .models import *
+from .serializers import *
+
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    # does POST and GET method calls
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    # Does the GET PUT DELETE method calls
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    # permission_class = [permissions.IsAuthenticated]
